7f7b57a0bde4e3d873790e349b262c971381e72a
Timmy1404 <timothypearman1@gmail.com>
2026-05-11 13:03:38 +0100

    added frontend task and tightened CORS
    - added run frontend task to .vscode and .example files
    
    - tightened CORS to only allow used methods and headers

--- backend/main.py
+++ backend/main.py
@@ -30,8 +30,8 @@ app.add_middleware(
         "http://localhost:5500",
     ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 
